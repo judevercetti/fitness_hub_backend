d05ea8f93a913c4b7c597d5ce625ca68e3957eac
judevercetti <judevercetti@gmail.com>
2023-10-27 00:37:07 +0300

    added static

--- config/urls.py
+++ config/urls.py
@@ -13,10 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
 from django.urls import include, path
-
-import api
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -24,4 +24,4 @@ urlpatterns = [
 
     path('admin/', admin.site.urls),
     
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
