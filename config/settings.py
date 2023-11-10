028f7c59a238e6e6fc4a4d394f38fd3cb2510fb7
judevercetti <judevercetti@gmail.com>
2023-11-10 15:00:46 +0300

    new settings

--- config/settings.py
+++ config/settings.py
@@ -140,8 +140,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    'ACCESS_TOKEN_LIFETIME': timedelta(days=1),
-    'REFRESH_TOKEN_LIFETIME': timedelta(days=2),
+    'ACCESS_TOKEN_LIFETIME': timedelta(days=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(days=5),
 }
 
 CORS_ALLOW_ALL_ORIGINS = True 
